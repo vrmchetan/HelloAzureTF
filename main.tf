4fcba35256f21be5d329a7e2753a5f9fe9655c75
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 16:12:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = abcd1234
-  resource_group_name   = test-resource
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+resource "azurerm_resource_group" "resourcegroups" {
+    name        = var.ResourceGroup
+    location    = var.Location
 }
