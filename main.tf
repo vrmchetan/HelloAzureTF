c03b3ae7db1d97c6d54b088754cbec8866060f81
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 15:06:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,41 @@
-resource "azurerm_resource_group" "resourcegroups" {
-    name        = var.ResourceGroup
-    location    = var.Location
+variable "prefix" {
+  default = "tfvmex"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "terra-vm"
-  location              = var.Location
-  resource_group_name   = var.ResourceGroup
-  network_interface_ids = terraform-github276
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
-}
