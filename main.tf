3a8dd78cdd15722b033abfe9e8d3da34a450f09d
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 15:57:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,4 @@ resource "azurerm_virtual_machine" "main" {
   resource_group_name   = test-resource
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
+}
