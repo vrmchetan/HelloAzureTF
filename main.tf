9c23c2422cdc3a674ed6e9a8abf80ca832d73192
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 12:43:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "azurerm_resource_group" "resourcegroups" {
     name        = var.ResourceGroup
     location    = var.Location
+}
+
+resource "azurerm_resource_group" "resourcegroups" {
+    name        = var.ResourceGroup
+    location    = var.Location
+}
