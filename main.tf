b3c4c187ec53473ec32eff27a67344ad4111f7af
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 15:31:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,30 @@
+variable "prefix" {
+  default = "tfvmex"
+}
+
 resource "azurerm_resource_group" "example" {
-  name     = "terra-resource"
+  name     = "${var.prefix}-resources"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "terra-network"
+  name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = West Europe
-  resource_group_name = terra-resource
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = terra-resource
-  virtual_network_name = terra-network
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "terra-interface-nic"
-  location            = West Europe
-  resource_group_name = terra-resource
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -30,9 +34,39 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "terra-vm"
-  location              = West Europe
-  resource_group_name   = terra-resource
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
 }
