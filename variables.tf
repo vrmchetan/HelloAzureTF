c69b4ae61f35643622a7d72dc958c5dc7900cc1e
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 12:41:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "ResourceGroup" {
     type        = string
     default     = "rg-connectivity-network-001"   
 }
+
+variable "Location" {
+    type        = string
+    default     = "WestEurope"  
+}
+
+variable "ResourceGroup" {
+    type        = string
+    default     = "rg-loganalytics-001"   
+}
