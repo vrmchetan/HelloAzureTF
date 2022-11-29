8e58146db9bdc92cd9df4a2ee76fd6458c0e3788
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-29 12:45:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -15,3 +15,21 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name     = "rg-terraform-state-001"
+    storage_account_name    = "cloudninjaterraformstate"
+    container_name          = "tfstate"
+    key                     = "GitHub-Terraform-rg-loganalytics-001"
+  }
+}
+provider "azurerm" {
+  features {}
+}
