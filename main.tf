7a08d73cfbb62236e1f4e5d0e24c02c6b004d146
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-30 11:29:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,12 @@ resource "azurerm_container_registry" "acr" {
   name                = "containerRegistry1"
   resource_group_name = var.ResourceGroup
   location            = var.Location
-  sku                 = "Premium"
+  sku                 = "Basic"
   admin_enabled       = false
   georeplications {
     location                = "North Europe"
     zone_redundancy_enabled = true
     tags                    = {}
   }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+  
 }
