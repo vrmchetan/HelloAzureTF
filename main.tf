167075e989a98ca0a4ee91402da33f193e9beb13
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-11-30 11:12:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ resource "azurerm_resource_group" "resourcegroups" {
 }
 resource "azurerm_container_registry" "acr" {
   name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group
-  location            = azurerm_resource_group
+  resource_group_name = var.ResourceGroup
+  location            = var.Location
   sku                 = "Premium"
   admin_enabled       = false
   georeplications {
-    location                = azurerm_resource_group
+    location                = var.Location
     zone_redundancy_enabled = true
     tags                    = {}
   }
   georeplications {
-    location                = azurerm_resource_group
+    location                = var.Location
     zone_redundancy_enabled = true
     tags                    = {}
   }
