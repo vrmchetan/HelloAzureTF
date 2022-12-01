a544373e549ce0bf3eef9a0081439bbd18e8e06d
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-12-01 13:11:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,13 @@
+
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 3.7.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.7.0"
     }
+
     azapi = {
-        source = "Azure/azapi"
+      source  = "Azure/azapi"
     }
-  } 
+  }
+}
