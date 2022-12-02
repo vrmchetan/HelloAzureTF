6974505186b1e4b1275cbbf87667be3ef9a802f8
chetan <chetan.kumar@appinventiv.com>
2022-12-02 17:04:06 +0530

    udpated main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "azurerm_container_group" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
   os_type             = "Linux"
 
   container {
