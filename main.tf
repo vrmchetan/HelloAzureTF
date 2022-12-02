91b35c31eb3cfb09e80aa817d62fca49f5776723
chetan <chetan.kumar@appinventiv.com>
2022-12-02 16:55:23 +0530

    udpated main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,15 @@
-resource "azurerm_resource_group" "resourcegroups" {
-    name        = var.ResourceGroup
-    location    = var.Location
-}
-resource "azurerm_container_registry" "acr" {
-  name                = "nitinkumar"
-  resource_group_name = azurerm_resource_group.resourcegroups.name
-  location            = azurerm_resource_group.resourcegroups.location
-  sku                 = "Basic"
-  admin_enabled       = false
-}
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "aca-env-terraform"
-  resource_group_name = azurerm_resource_group.resourcegroups.name
-  location            = azurerm_resource_group.resourcegroups.location
-  sku                 = "PerGB2018"
-  retention_in_days   = 90 
-}
-# creating aca environment
-resource "azapi_resource" "aca_env" {
-     type       = "Microsoft.App/managedEnvironments@2022-03-01"
-     parent_id  = azurerm_resource_group.resourcegroups.id
-     location = var.Location
-     name = "aca-env-terraform"
-     schema_validation_enabled = false
-     
-     body = jsonencode({
-        properties = {
-            appLogsConfiguration = {
-                destination = "log-analytics"
-                logAnalyticsConfiguration = {
-                    customerId = azurerm_log_analytics_workspace.law.workspace_id 
-                    sharedKey  = azurerm_log_analytics_workspace.law.primary_shared_key
-                }
-            }
-        }
-
-    })
-
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-
-#creating the aca
-resource "azapi_resource" "container_app" {
-    type       = "Microsoft.App/containerApps@2022-03-01"
-    parent_id  = azurerm_resource_group.resourcegroups.id
-    location   = azurerm_resource_group.resourcegroups.location
-    name       = "aca-test-environments"
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
 
   container {
     name   = "hello-world"
@@ -57,7 +21,16 @@ resource "azapi_resource" "container_app" {
       port     = 443
       protocol = "TCP"
     }
-  
-}
-}
+  }
 
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
