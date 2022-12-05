352abaa7ab4a8b678a4ba32c21d169dacdd5fd7f
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-12-05 17:28:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,44 @@
 resource "azurerm_resource_group" "example" {
-  name     = "nitikumarsinghh"
+  name     = "terra-resource-001"
   location = "West Europe"
 }
 
+resource "azurerm_container_registry" "acr" {
+    name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+  admin_enabled       = false
+}
+
+resource "azurerm_container_app_env" "example" {
+  name                   = "terra-containerapp-env"
+  location               = azurerm_resource_group.example.location
+  resource_group_name    = azurerm_resource_group.example.name
+  logs_workspace_id      = "logsworkspaceclientid"
+  logs-workspace-key     = "logsworkspaceclientsecret"
+}
+
+resource "azurerm_container_app" "example" {
+  name                   = "containerapp"
+  resource_group_name    = "resourcegroupname"
+  environment            = "azurerm_container_app_env.example.environment"
+  image                  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+  target_port            = 80
+  ingress                = "external"
+  query                  = "configuration.ingress.fqdn"
+}
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "terra-acctest-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_container_group" "example" {
-  name                = "nitikumarsinghh"
+  name                = "terra-acg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   ip_address_type     = "Public"
@@ -32,20 +66,3 @@ resource "azurerm_container_group" "example" {
   tags = {
     environment = "testing"
   }
-  # Create a resource group
-  resource "azurerm_resource_group" "rg" {
-  name     = "rg"
-  location = "West Europe"
-  tags =  {
-  env = "dev"
-  cost = "TA102AXY"
-  }
-}
-resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry543456"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Standard"
-  admin_enabled       = true
- }
-}
