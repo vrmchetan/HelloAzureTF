e49a233270ab9d8a0dae45e7e45230529fd137f3
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-12-06 11:47:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_log_analytics_workspace" "example" {
     }
 
 resource "azurerm_container_registry" "example" {
-    name                = "containerRegistry1"
+    name                = "test-alpa-containerRegistry1"
     resource_group_name = azurerm_resource_group.example.name
     location            = azurerm_resource_group.example.location
     sku                 = "Premium"
