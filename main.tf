82373be4cc981faa75b0af732891c5ae8aa9d4c4
vrmchetan <45027816+vrmchetan@users.noreply.github.com>
2022-12-06 12:26:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_container_group" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "terra-container-group-instance"
   os_type             = "Linux"
 
   container {
